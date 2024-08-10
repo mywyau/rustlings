31743e0fa9636cff57a7345f91c30256ff6aa1f3
mywyau <47248104+mywyau@users.noreply.github.com>
2024-08-10 22:20:29 +0100

    excercise-2

--- target/debug/move_semantics1.d
+++ target/debug/move_semantics1.d
@@ -1,1 +1,1 @@
-/Users/michaelyau/RustroverProjects/rustlings/rustlings/target/debug/move_semantics1: /Users/michaelyau/RustroverProjects/rustlings/rustlings/exercises/06_move_semantics/move_semantics1.rs
+/Users/michaelyau/RustroverProjects/rustlings/target/debug/move_semantics1: /Users/michaelyau/RustroverProjects/rustlings/exercises/06_move_semantics/move_semantics1.rs
